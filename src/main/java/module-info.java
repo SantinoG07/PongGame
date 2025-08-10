cb280302e78ea90a5ea56aefd3725d131dfa525d
SantinoG07 <santino.gennuso.m@gmail.com>
2025-08-09 22:14:43 -0300

    Primer commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module org.example.pong {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens org.example.pong to javafx.fxml;
+    exports org.example.pong;
+}
---
+++ src/main/java/org/example/pong/HelloApplication.java
@@ -0,0 +1,25 @@
+package org.example.pong;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Game.fxml"));
+        Scene scene = new Scene(fxmlLoader.load()); // quito 320, 240
+        stage.setScene(scene);
+        stage.setFullScreen(true);
+        stage.setTitle("Pong Game");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
